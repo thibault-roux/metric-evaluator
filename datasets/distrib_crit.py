8a102da08df0a686b6b9394b2b4091d05f0b21e1
thibault-roux <thibault.roux1@laposte.net>
2023-04-20 09:31:27 +0200

    Compute scores work

--- datasets/distrib_crit.py
+++ datasets/distrib_crit.py
@@ -32,6 +32,51 @@ def semdist(ref, hyp, memory):
         sd = 1
     return (1 - sd)*100
 
+def semdist_global(refs, hypsA, hypsB):
+    print("Computing SemDist...")
+    semdist_model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+    semdist_results = []
+    for i in range(len(refs)):
+        ref = refs[i]
+        hypA = hypsA[i]
+        hypB = hypsB[i]
+        semdist_results.append([semdist(ref, hypA, semdist_model), semdist(ref, hypB, semdist_model)])
+
+    # save results 
+    with open("pickle/semdist_results.pkl", "wb") as file:
+        pickle.dump(semdist_results, file)
+
+def bertscore_global(refs, hypsA, hypsB):
+    print("Computing BertScore...")
+    # nphyps = np.array(hyps)
+    # P, R, F1_A = score(refs, nphyps[:,0], lang="fr", verbose=True)
+    # P, R, F1_B = score(refs, nphyps[:,1], lang="fr", verbose=True)
+    P, R, F1_A = score(refs, hypsA, lang="fr", verbose=True)
+    P, R, F1_B = score(refs, hypsB, lang="fr", verbose=True)
+    # write code to convert F1 to a list where each value is substracted by 1 and multiplied by 100
+    F1_A = [((1 - f1) * 100) for f1 in F1_A]
+    F1_B = [((1 - f1) * 100) for f1 in F1_B]
+    
+    bertscore_results = []
+    for i in range(len(F1_A)):
+        bertscore_results.append([F1_A[i].item(), F1_B[i].item()])
+        
+    # save results 
+    with open("pickle/bertscore_results.pkl", "wb") as file:
+        pickle.dump(bertscore_results, file)
+
+
+def wer_global(refs, hypsA, hypsB):
+    print("Computing WER...")
+    werA = [wer(ref, hyp) for ref, hyp in zip(refs, hypsA)]
+    werB = [wer(ref, hyp) for ref, hyp in zip(refs, hypsB)]
+    wer_results = []
+    for i in range(len(werA)):
+        wer_results.append([werA[i], werB[i]])
+
+    # save results 
+    with open("pickle/wer_results.pkl", "wb") as file:
+        pickle.dump(wer_results, file)
     
 
 def compute_metrics():
@@ -47,55 +92,13 @@ def compute_metrics():
             hypsB.append(line[3])
 
 
-    print("Computing SemDist...")
-    semdist_model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
-    semdist_results = []
-    for i in range(len(refs)):
-        ref = refs[i]
-        hypA = hypsA[i]
-        hypB = hypsB[i]
-        semdist_results.append([semdist(ref, hypA, semdist_model), semdist(ref, hypB, semdist_model)])
-
-    # save results 
-    with open("pickle/semdist_results.pkl", "wb") as file:
-        pickle.dump(semdist_results, file)
-
-    input("Continuer ?")
-
-
-    print("Computing BertScore...")
-    # nphyps = np.array(hyps)
-    # P, R, F1_A = score(refs, nphyps[:,0], lang="fr", verbose=True)
-    # P, R, F1_B = score(refs, nphyps[:,1], lang="fr", verbose=True)
-    P, R, F1_A = score(refs, hypsA, lang="fr", verbose=True)
-    P, R, F1_B = score(refs, hypsB, lang="fr", verbose=True)
-    # write code to convert F1 to a list where each value is substracted by 1 and multiplied by 100
-    F1_A = [((1 - f1) * 100) for f1 in F1_A]
-    F1_B = [((1 - f1) * 100) for f1 in F1_B]
-    
-    bertscore_results = []
-    for i in range(len(F1_A)):
-        bertscore_results.append([F1_A[i], F1_B[i]])
-
-    # save results 
-    with open("pickle/bertscore_results.pkl", "wb") as file:
-        pickle.dump(bertscore_results, file)
-
-
-    print("Computing WER...")
-    werA = [wer(ref, hyp) for ref, hyp in zip(ref, hypsA)]
-    werB = [wer(ref, hyp) for ref, hyp in zip(ref, hypsB)]
-    wer_results = []
-    for i in range(len(werA)):
-        wer_results.append([werA[i], werB[i]])
-
-    # save results 
-    with open("pickle/wer_results.pkl", "wb") as file:
-        pickle.dump(wer_results, file)
+    semdist_global(refs, hypsA, hypsB)
+    bertscore_global(refs, hypsA, hypsB)
+    wer_global(refs, hypsA, hypsB)
 
 
 if __name__ == "__main__":
-    compute_metrics()
+    # compute_metrics()
 
     semdist_results = pickle.load(open("pickle/semdist_results.pkl", "rb"))
     bertscore_results = pickle.load(open("pickle/bertscore_results.pkl", "rb"))
